3b31a5e042346b21f6b0127be29a9a05feace54e
Haruka <mrx@hcc.im>
2023-05-19 17:00:20 +0900

    change formatting for large numbers

--- webui/template.py
+++ webui/template.py
@@ -13,15 +13,33 @@ def format_time(epoch):
     time_str = datetime.datetime.fromtimestamp(epoch, tz=datetime.timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
     return f"""<span class="time">{time_str}</span>"""
 
+def format_number(number, decimal_places=2):
+    if not isinstance(number, Decimal):
+        number = Decimal(number)
+    integer = str(int(number))
+    integer_parts = []
+    for i in range(len(integer) - 1, -1, -3):
+        integer_parts.append(integer[max(i - 2, 0):i + 1])
+    integer_parts.reverse()
+    decimal = str(number % 1)[2:2 + decimal_places]
+    decimal_parts = []
+    for i in range(0, len(decimal), 3):
+        decimal_parts.append(decimal[i:i + 3])
+    if not decimal_parts:
+        return '<span class="formatted-number">' + \
+            "".join(map(lambda x: f'<span class="number-part">{x}</span>', integer_parts)) + \
+            '</span>'
+
+    return '<span class="formatted-number">' + \
+        "".join(map(lambda x: f'<span class="number-part">{x}</span>', integer_parts)) + \
+        '<span class="number-dot">.</span>' + \
+        "".join(map(lambda x: f'<span class="number-part">{x}</span>', decimal_parts)) + \
+        '</span>'
+
 def format_aleo_credit(mc):
     if mc == "-":
         return "-"
-    return "{:,}".format(Decimal(mc) / 1_000_000)
-
-def format_number(number):
-    if isinstance(number, Decimal):
-        return f"{number:,.2f}"
-    return "{:,}".format(number)
+    return format_number(Decimal(mc) / 1_000_000, 6)
 
 templates.env.filters["get_env"] = get_env
 templates.env.filters["format_time"] = format_time
