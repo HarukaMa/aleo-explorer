bac547e9ff77dca43ef2a143918a778875da5acb
Haruka <mrx@hcc.im>
2023-07-02 19:54:16 +0900

    slight changes to api quota middleware

--- middleware/api_quota.py
+++ middleware/api_quota.py
@@ -12,6 +12,8 @@ class RequestTiming:
     start_ns: int = 0
     end_ns: int = 0
 
+COST_TIMEOUT = -1
+COST_UNKNOWN = -2
 
 class APIQuotaMiddleware:
     def __init__(self, app: ASGIApp, *, max_call_time = 10.0, recover_rate = 0.1) -> None:
@@ -19,34 +21,39 @@ class APIQuotaMiddleware:
         self.max_call_time = max_call_time
         self.recover_rate = recover_rate
         self.ip_remaining_time = defaultdict(lambda: (max_call_time, -1.0, 0))
+        self.ip_remaining_time_lock = asyncio.Lock()
 
-    def start_call(self, ip):
-        remaining, last_call, outstanding_call = self.ip_remaining_time[ip]
-        print(f"ip {ip} has quota {remaining}s, last call {last_call}, outstanding call {outstanding_call}")
-        if outstanding_call == 0 and last_call != -1:
-            quota = remaining + (time.monotonic() - last_call) * self.recover_rate
-            if quota > self.max_call_time:
-                quota = self.max_call_time
-            print(f"ip {ip} quota recovered to {quota}")
-        else:
-            quota = remaining
-        # save current quota subtract 1 second right now to avoid flood attack
-        self.ip_remaining_time[ip] = (quota - 1, last_call, outstanding_call + 1)
+    async def start_call(self, ip):
+        async with self.ip_remaining_time_lock:
+            remaining, last_call, outstanding_call = self.ip_remaining_time[ip]
+            print(f"ip {ip} has quota {remaining}s, last call {last_call}, outstanding call {outstanding_call}")
+            if outstanding_call == 0 and last_call != -1:
+                quota = remaining + (time.monotonic() - last_call) * self.recover_rate
+                if quota > self.max_call_time:
+                    quota = self.max_call_time
+                print(f"ip {ip} quota recovered to {quota}")
+            else:
+                quota = remaining
+            # save current quota subtract 1 second right now to avoid flood attack
+            self.ip_remaining_time[ip] = (quota - 1, last_call, outstanding_call + 1)
         return quota
 
     def get_quota_for_header(self, ip, cost):
         remaining, _, outstanding_call = self.ip_remaining_time[ip]
         return remaining - cost + outstanding_call
 
-    def end_call(self, ip, cost):
-        remaining, _, outstanding_call = self.ip_remaining_time[ip]
-        if cost == -1:
-            # timeout
-            self.ip_remaining_time[ip] = (0, time.monotonic(), outstanding_call - 1)
-        elif cost == -2:
-            # honestly, unknown exception, refunding time used
-            self.ip_remaining_time[ip] = (remaining + 1, time.monotonic(), outstanding_call - 1)
-        else:
+    async def end_call(self, ip, cost):
+        async with self.ip_remaining_time_lock:
+            remaining, _, outstanding_call = self.ip_remaining_time[ip]
+            if cost == COST_TIMEOUT:
+                # don't add the 1 sec back as the user has depleted the quota
+                cost = remaining + 1
+            elif cost == COST_UNKNOWN:
+                # honestly, unknown exception, refunding time used
+                cost = 0
+            elif cost < 0:
+                # just in case
+                cost = 0
             self.ip_remaining_time[ip] = (remaining - cost + 1, time.monotonic(), outstanding_call - 1)
         remaining, last_call, outstanding_call = self.ip_remaining_time[ip]
         print(f"ip {ip} used {cost}s, remaining {remaining}s, last call {last_call}, outstanding call {outstanding_call}")
@@ -57,12 +64,12 @@ class APIQuotaMiddleware:
             return await self.app(scope, receive, send)
 
         ip = scope["client"][0]
-        remaining = self.start_call(ip)
+        remaining = await self.start_call(ip)
         timing = RequestTiming()
-        cost = -2 # final catch if try except borked
+        cost = COST_UNKNOWN # final catch if try except borked
         try:
-            receive = functools.partial(self.timing_receive, timing=timing, receive=receive)
-            send = functools.partial(self.timing_send, scope=scope, timing=timing, send=send)
+            receive = functools.partial(self.wrapped_receive, timing=timing, receive=receive)
+            send = functools.partial(self.wrapped_send, scope=scope, timing=timing, send=send)
             timing.start_ns = time.perf_counter_ns()
             await asyncio.wait_for(self.app(scope, receive, send), timeout=remaining)
             cost = (timing.end_ns - timing.start_ns) / 1e9
@@ -72,25 +79,28 @@ class APIQuotaMiddleware:
             }
             response = JSONResponse({"error": "Request timed out - API quota exceeded"}, status_code=429, headers=headers)
             await response(scope, receive, send)
-            cost = -1
+            cost = COST_TIMEOUT
         except Exception as e:
             # internal bug, refunding time used
             timing.end_ns = timing.start_ns
             response = JSONResponse({"error": f"Server error: {e}. Please report with the feedback feature."}, status_code=500)
             await response(scope, receive, send)
             cost = 0
+            print(f"Unknown exception {e} in APIQuotaMiddleware")
+            import traceback
+            traceback.print_exc()
         finally:
-            self.end_call(ip, cost)
+            await self.end_call(ip, cost)
 
     # noinspection PyMethodMayBeStatic
-    async def timing_receive(self, timing: RequestTiming, receive: Receive) -> Message:
+    async def wrapped_receive(self, timing: RequestTiming, receive: Receive) -> Message:
         message = await receive()
         if not message.get("more_body", False):
             timing.start_ns = time.perf_counter_ns()
         return message
 
 
-    async def timing_send(self, message: Message, scope: Scope, timing: RequestTiming, send: Send) -> None:
+    async def wrapped_send(self, message: Message, scope: Scope, timing: RequestTiming, send: Send) -> None:
         if timing.end_ns == 0:
             timing.end_ns = time.perf_counter_ns()
         if message["type"] != "http.response.start":
