0718c9ce0fcdeec7c8804b86760a8718c434d774
snowtiger <snowtigersoft@126.com>
2023-07-20 00:45:58 +0800

    Support run explorer from docker compose

--- webui/webui.py
+++ webui/webui.py
@@ -180,7 +180,9 @@ app = Starlette(
 
 
 async def run():
-    config = uvicorn.Config("webui:app", reload=True, log_level="info", port=int(os.environ.get("PORT", 8000)))
+    host = os.environ.get("HOST", "127.0.0.1")
+    port = int(os.environ.get("PORT", 8000))
+    config = uvicorn.Config("webui:app", reload=True, log_level="info", host=host, port=port)
     logging.getLogger("uvicorn.access").handlers = []
     server = UvicornServer(config=config)
 
