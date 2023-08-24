9716fbaa2da42e9d18a3f1ed240d10267b65e1a2
Haruka <mrx@hcc.im>
2023-08-24 21:15:26 +0900

    add error route handling

--- webui/error_routes.py
+++ webui/error_routes.py
@@ -4,15 +4,30 @@ from .template import templates
 
 
 async def bad_request(request: Request, exc: Exception):
-    return templates.TemplateResponse('400.jinja2', {'request': request, "exc": exc}, status_code=400) # type: ignore
+    is_htmx = request.scope["htmx"].is_htmx()
+    if is_htmx:
+        template = "htmx/400.jinja2"
+    else:
+        template = "400.jinja2"
+    return templates.TemplateResponse(template, {'request': request, "exc": exc}, status_code=400) # type: ignore
 
 
 async def not_found(request: Request, exc: Exception):
-    return templates.TemplateResponse('404.jinja2', {'request': request, "exc": exc}, status_code=404) # type: ignore
+    is_htmx = request.scope["htmx"].is_htmx()
+    if is_htmx:
+        template = "htmx/404.jinja2"
+    else:
+        template = "404.jinja2"
+    return templates.TemplateResponse(template, {'request': request, "exc": exc}, status_code=404) # type: ignore
 
 
 async def internal_error(request: Request, exc: Exception):
-    return templates.TemplateResponse('500.jinja2', {'request': request, "exc": exc}, status_code=500) # type: ignore
+    is_htmx = request.scope["htmx"].is_htmx()
+    if is_htmx:
+        template = "htmx/500.jinja2"
+    else:
+        template = "500.jinja2"
+    return templates.TemplateResponse(template, {'request': request, "exc": exc}, status_code=500) # type: ignore
 
 
 async def cloudflare_error_page(request: Request):
