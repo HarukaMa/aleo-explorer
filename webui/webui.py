d5295c23290a6158bea1a1ea3f23b89deb8e992e
Haruka <mrx@hcc.im>
2023-10-01 15:08:05 +0900

    change to template

--- webui/webui.py
+++ webui/webui.py
@@ -136,8 +136,8 @@ async def privacy_route(request: Request):
 async def robots_route(_: Request):
     return FileResponse("webui/robots.txt", headers={'Cache-Control': 'public, max-age=3600'})
 
-async def down_route(_: Request):
-    return FileResponse("webui/templates/down.jinja2", headers={'Cache-Control': 'no-cache'})
+async def down_route(request: Request):
+    return templates.TemplateResponse("down.jinja2", ctx={"request": request}, headers={'Cache-Control': 'no-cache'})
 
 routes = [
     Route("/{path:path}", down_route),
