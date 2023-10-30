967111651f7d14325bcfffa881396d7d3ab8d765
Haruka <mrx@hcc.im>
2023-10-31 04:15:38 +0900

    fix migrate script again

--- migration_2.sql
+++ migration_2.sql
@@ -99,19 +99,19 @@ begin
         select t.id, t.type, t.index from finalize_operation t where t.confirmed_transaction_id = confirmed_transaction_db_id
         loop
             if type = 'InitializeMapping' then
-                select t.mapping_id from finalize_operation_initialize_mapping t where t.id = finalize_operation_db_id into mapping_id;
+                select t.mapping_id from finalize_operation_initialize_mapping t where t.finalize_operation_id = finalize_operation_db_id into mapping_id;
                 return next;
             elsif type = 'InsertKeyValue' then
-                select t.mapping_id, t.key_id, t.value_id from finalize_operation_insert_kv t where t.id = finalize_operation_db_id into mapping_id, key_id, value_id;
+                select t.mapping_id, t.key_id, t.value_id from finalize_operation_insert_kv t where t.finalize_operation_id = finalize_operation_db_id into mapping_id, key_id, value_id;
                 return next;
             elsif type = 'UpdateKeyValue' then
-                select t.mapping_id, t.key_id, t.value_id from finalize_operation_update_kv t where t.id = finalize_operation_db_id into mapping_id, key_id, value_id;
+                select t.mapping_id, t.key_id, t.value_id from finalize_operation_update_kv t where t.finalize_operation_id = finalize_operation_db_id into mapping_id, key_id, value_id;
                 return next;
             elsif type = 'RemoveKeyValue' then
-                select t.mapping_id from finalize_operation_remove_kv t where t.id = finalize_operation_db_id into mapping_id;
+                select t.mapping_id from finalize_operation_remove_kv t where t.finalize_operation_id = finalize_operation_db_id into mapping_id;
                 return next;
             elsif type = 'RemoveMapping' then
-                select t.mapping_id from finalize_operation_remove_mapping t where t.id = finalize_operation_db_id into mapping_id;
+                select t.mapping_id from finalize_operation_remove_mapping t where t.finalize_operation_id = finalize_operation_db_id into mapping_id;
                 return next;
             else
                 raise exception 'unsupported finalize operation type: %', type;
