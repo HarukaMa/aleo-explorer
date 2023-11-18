b1c76bd6f14cd26ba20aa1d8740bfc3325a133bf
Haruka <mrx@hcc.im>
2023-11-19 04:10:50 +0900

    fix timezone not applying on htmx load

--- webui/static/base_script_v1.js
+++ webui/static/base_script_v1.js
@@ -1,35 +1,41 @@
 
-customElements.define(
-    "advanced-link",
-    class extends HTMLElement {
-        constructor() {
-            super();
-            this.addEventListener("advanced-toggle", (e) => {
-                const show = e.detail;
-                if (show) {
-                    const query_string = Array.from(this.attributes).map((attr) => {
-                        return `${attr.name}=${attr.value}`;
-                    }).reduce((a, b) => {
-                        return `${a}&${b}`;
-                    });
-                    this.innerHTML = `<a class="advanced-link" href="/advanced?${query_string}">${this.innerHTML}</a>`;
-                } else {
-                    if (this.children[0].tagName === "A") {
-                        this.innerHTML = this.children[0].innerHTML;
+if (customElements.get("advanced-link") === undefined) {
+    customElements.define(
+        "advanced-link",
+        class extends HTMLElement {
+            constructor() {
+                super();
+                this.addEventListener("advanced-toggle", (e) => {
+                    const show = e.detail;
+                    if (show) {
+                        const query_string = Array.from(this.attributes).map((attr) => {
+                            return `${attr.name}=${attr.value}`;
+                        }).reduce((a, b) => {
+                            return `${a}&${b}`;
+                        });
+                        this.innerHTML = `<a class="advanced-link" href="/advanced?${query_string}">${this.innerHTML}</a>`;
+                    } else {
+                        if (this.children[0].tagName === "A") {
+                            this.innerHTML = this.children[0].innerHTML;
+                        }
                     }
-                }
-            })
-        }
+                })
+            }
 
-    }
-)
+        }
+    )
+}
 
 function utc_to_local() {
     const times = document.getElementsByClassName("time")
     for (const time of times) {
+        if (time.dataset.is_local) {
+            continue
+        }
         const text = time.innerText
         const date = new Date(Date.parse(text.replace(" ", "T") + "Z"))
         time.innerText = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`
+        time.dataset.is_local = true
     }
 }
 
@@ -42,7 +48,7 @@ function local_to_utc() {
     }
 }
 
-const timezone_setting_element = document.getElementById("timezone-setting")
+timezone_setting_element = document.getElementById("timezone-setting")
 
 function toggle_timezone() {
     if (localStorage.getItem("timezone") === "local") {
@@ -57,20 +63,26 @@ function toggle_timezone() {
 }
 
 
-let timezone_setting = localStorage.getItem("timezone")
+timezone_setting = localStorage.getItem("timezone")
 if (timezone_setting === null) {
     timezone_setting = "utc"
     localStorage.setItem("timezone", "utc")
 }
 
-if (timezone_setting === "local") {
-    utc_to_local()
-    timezone_setting_element.innerText = "Local"
-} else {
-    timezone_setting_element.innerText = "UTC"
+function apply_timezone() {
+    if (timezone_setting === "local") {
+        utc_to_local()
+        timezone_setting_element.innerText = "Local"
+    } else {
+        timezone_setting_element.innerText = "UTC"
+    }
 }
 
-let advanced_setting = "false"
+(() => {
+    apply_timezone()
+})()
+
+advanced_setting = "false"
 if (advanced_setting === null) {
     advanced_setting = false
     localStorage.setItem("advanced", "false")
@@ -78,7 +90,7 @@ if (advanced_setting === null) {
     advanced_setting = JSON.parse(advanced_setting)
 }
 
-const advanced_setting_element = document.getElementById("advanced-setting")
+advanced_setting_element = document.getElementById("advanced-setting")
 
 advanced_setting_element.innerText = "Off"
 dispatch_advanced_event()
