41b94c7f58f2d2f0903c2c6883e62c63d7891f2a
Haruka <mrx@hcc.im>
2024-04-29 15:44:25 +0900

    update network params

--- node/canary/param.py
+++ node/canary/param.py
@@ -12,7 +12,7 @@ def load_program(program_id: str) -> Program:
 class Canary:
     edition = u16()
     network_id = u16()
-    version = u32(50)
+    version = u32(51)
 
     genesis_block = Block.load(BytesIO(open(os.path.join(os.path.dirname(__file__), "block.genesis"), "rb").read()))
     dev_genesis_block = Block.load(BytesIO(open(os.path.join(os.path.dirname(__file__), "dev.genesis"), "rb").read()))
