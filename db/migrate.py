e5e8f8c979f697e1326a2f88b7c6feef43d29d2c
Haruka <mrx@hcc.im>
2024-07-17 02:06:13 +0900

    add migration to fix stats

--- db/migrate.py
+++ db/migrate.py
@@ -6,6 +6,7 @@ from typing import Awaitable, LiteralString
 import psycopg
 import psycopg.sql
 from psycopg.rows import DictRow
+from redis.asyncio.client import Redis
 
 from aleo_types import *
 from explorer.types import Message as ExplorerMessage
@@ -17,11 +18,12 @@ class DatabaseMigrate(DatabaseBase):
 
     # migration methods
     async def migrate(self):
-        migrations: list[tuple[int, Callable[[psycopg.AsyncConnection[DictRow]], Awaitable[None]]]] = [
+        migrations: list[tuple[int, Callable[[psycopg.AsyncConnection[DictRow], Redis[str]], Awaitable[None]]]] = [
             (1, self.migrate_1_add_rejected_original_id),
             (2, self.migrate_2_set_on_delete_cascade),
             (3, self.migrate_3_fix_finalize_operation_function),
-            (4, self.migrate_4_add_solution_id)
+            (4, self.migrate_4_add_solution_id),
+            (5, self.migrate_5_full_public_balance_stats_tracking),
         ]
         async with self.pool.connection() as conn:
             async with conn.cursor() as cur:
@@ -32,14 +34,14 @@ class DatabaseMigrate(DatabaseBase):
                         if res is None or res['count'] == 0:
                             print(f"DB migrating {migrated_id}")
                             async with conn.transaction():
-                                await method(conn)
+                                await method(conn, self.redis)
                                 await cur.execute("INSERT INTO _migration (migrated_id) VALUES (%s)", (migrated_id,))
                 except Exception as e:
                     await self.message_callback(ExplorerMessage(ExplorerMessage.Type.DatabaseError, e))
                     raise
 
     @staticmethod
-    async def migrate_1_add_rejected_original_id(conn: psycopg.AsyncConnection[DictRow]):
+    async def migrate_1_add_rejected_original_id(conn: psycopg.AsyncConnection[DictRow], _: Redis[str]):
         async with conn.cursor() as cur:
             await cur.execute(
                 "SELECT ct.id, b.id as block_id FROM confirmed_transaction ct "
@@ -64,7 +66,7 @@ class DatabaseMigrate(DatabaseBase):
                         )
 
     @staticmethod
-    async def migrate_2_set_on_delete_cascade(conn: psycopg.AsyncConnection[DictRow]):
+    async def migrate_2_set_on_delete_cascade(conn: psycopg.AsyncConnection[DictRow], _: Redis[str]):
         # https://stackoverflow.com/a/74476119
         try:
             await conn.execute("""
@@ -92,12 +94,12 @@ WHERE tables.oid = pg_trigger.tgrelid
             raise
 
     @staticmethod
-    async def migrate_3_fix_finalize_operation_function(conn: psycopg.AsyncConnection[DictRow]):
+    async def migrate_3_fix_finalize_operation_function(conn: psycopg.AsyncConnection[DictRow], _: Redis[str]):
         await conn.execute(cast(LiteralString, open("migration_3.sql").read()))
 
 
     @staticmethod
-    async def migrate_4_add_solution_id(conn: psycopg.AsyncConnection[DictRow]):
+    async def migrate_4_add_solution_id(conn: psycopg.AsyncConnection[DictRow], _: Redis[str]):
         # add column
         try:
             async with conn.transaction():
@@ -130,3 +132,77 @@ WHERE tables.oid = pg_trigger.tgrelid
                         "UPDATE solution SET solution_id = %s WHERE id = %s",
                         (str(solution_id), row["id"])
                     )
+
+    @staticmethod
+    async def migrate_5_full_public_balance_stats_tracking(conn: psycopg.AsyncConnection[DictRow], redis: Redis[str]):
+        async with conn.cursor() as cur:
+            # all additional finalize executions to track
+            await cur.execute(
+                "SELECT b.height, f.function_name, f.id FROM future f "
+                "JOIN transition_output_future tof on f.transition_output_future_id = tof.id "
+                "JOIN transition_output to_ on tof.transition_output_id = to_.id "
+                "JOIN transition t on to_.transition_id = t.id "
+                "JOIN transaction_execute te on t.transaction_execute_id = te.id "
+                "JOIN transaction tx on te.transaction_id = tx.id "
+                "JOIN confirmed_transaction ct on tx.confirmed_transaction_id = ct.id "
+                "JOIN block b on ct.block_id = b.id "
+                "WHERE f.program_id = 'credits.aleo' "
+                "AND f.function_name IN ('bond_validator', 'bond_public', 'claim_unbond_public') "
+                "AND ct.type = 'AcceptedExecute'"
+            )
+            futures = await cur.fetchall()
+            for future in futures:
+                # future arguments
+                await cur.execute(
+                    "SELECT fa.plaintext FROM future_argument fa "
+                    "JOIN explorer.future f on f.id = fa.future_id "
+                    "WHERE f.id = %s "
+                    "ORDER BY fa.id",
+                    (future["id"],)
+                )
+                arguments = await cur.fetchall()
+                if future["function_name"] in ("bond_validator", "bond_public"):
+                    if future["function_name"] == "bond_validator":
+                        # bond_validator(val, withdraw, amount, commission)
+                        validator_plaintext = cast(LiteralPlaintext, Plaintext.load(BytesIO(arguments[0]["plaintext"])))
+                        validator_address = str(validator_plaintext)
+                        amount = int(cast(u64, cast(LiteralPlaintext, Plaintext.load(BytesIO(arguments[2]["plaintext"]))).literal.primitive))
+                        await redis.hincrby("address_transfer_out", validator_address, amount)
+                    elif future["function_name"] == "bond_public":
+                        # bond_public(staker, val, withdraw, amount)
+                        staker_plaintext = cast(LiteralPlaintext, Plaintext.load(BytesIO(arguments[0]["plaintext"])))
+                        staker_address = str(staker_plaintext)
+                        amount = int(cast(u64, cast(LiteralPlaintext, Plaintext.load(BytesIO(arguments[3]["plaintext"]))).literal.primitive))
+                        await redis.hincrby("address_transfer_out", staker_address, amount)
+                elif future["function_name"] == "claim_unbond_public":
+                    # claim_unbond_public(staker)
+                    staker_plaintext = cast(LiteralPlaintext, Plaintext.load(BytesIO(arguments[0]["plaintext"])))
+                    staker_address = str(staker_plaintext)
+                    # get last unbonding mapping value
+                    unbonding_key_id = cached_get_key_id("credits.aleo", "unbonding", staker_plaintext.dump())
+                    await cur.execute(
+                        "SELECT value FROM mapping_history "
+                        "WHERE height < %s AND key_id = %s "
+                        "ORDER BY height DESC LIMIT 1",
+                        (future["height"], unbonding_key_id)
+                    )
+                    row = await cur.fetchone()
+                    if row is None:
+                        raise RuntimeError(f"unbonding mapping value not found for {staker_address} at height {future['height']}")
+                    unbonding_value = Value.load(BytesIO(row["value"]))
+                    unbonding = cast(StructPlaintext, cast(PlaintextValue, unbonding_value).plaintext)
+                    amount = int(cast(u64, cast(LiteralPlaintext, unbonding["microcredits"]).literal.primitive))
+                    # get last withdraw mapping value
+                    withdraw_key_id = cached_get_key_id("credits.aleo", "withdraw", staker_plaintext.dump())
+                    await cur.execute(
+                        "SELECT value FROM mapping_history "
+                        "WHERE height < %s AND key_id = %s "
+                        "ORDER BY height DESC LIMIT 1",
+                        (future["height"], withdraw_key_id)
+                    )
+                    row = await cur.fetchone()
+                    if row is None:
+                        raise RuntimeError(f"withdraw mapping value not found for {staker_address} at height {future['height']}")
+                    withdraw_value = Value.load(BytesIO(row["value"]))
+                    withdraw_address = str(cast(LiteralPlaintext, cast(PlaintextValue, withdraw_value).plaintext))
+                    await redis.hincrby("address_transfer_in", withdraw_address, amount)
