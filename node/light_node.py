b42ae760a284d7d58bf70a2d57c1e71782e97f61
Haruka <mrx@hcc.im>
2026-01-15 04:58:37 +0900

    add light node peer response log

--- node/light_node.py
+++ node/light_node.py
@@ -274,6 +274,7 @@ class LightNode:
                             peer_types[p[0]] = NodeType[p[1]]
                 except Exception:
                     pass
+            self.log(f"Peers: {msg.peers}")
             for peer in msg.peers:
                 if str(peer) in peer_types:
                     peer_type = peer_types[str(peer)]
